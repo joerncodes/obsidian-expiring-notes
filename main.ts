271da7eb1f63a48d7498a8e88946b232e67d5087
Joern Meyer <joern.meyer@kernpunkt.de>
2021-11-14 15:27:44 +0100

    Initial commit

--- main.ts
+++ main.ts
@@ -1,81 +1,98 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Moment } from 'moment';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile, FrontMatterCache, DropdownComponent, normalizePath } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface ExpiringNotesSettings {
+	frontmatterKey: string;
+	behavior: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: ExpiringNotesSettings = {
+	frontmatterKey: 'expires',
+	behavior: 'delete'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class ExpiringNotesPlugin extends Plugin {
+	settings: ExpiringNotesSettings;
+
+	async checkForExpiredNotes() {
+		let expiredNotes = this.collectExpiredNotes();
+
+		if (this.settings.behavior === 'delete') {
+
+			expiredNotes.forEach((file) => {
+					this.deleteExpiredNote(file);
+			});
+
+			new Notice('Deleted ' + expiredNotes.length + ' expired note(s).');
+		}
+
+		if (this.settings.behavior === 'trash') {
+
+			expiredNotes.forEach((file) => {
+				this.trashExpiredNote(file);
+			});
+
+			new Notice('Trashed  ' + expiredNotes.length + ' expired note(s).');
+		}
+	}
+
+	async trashExpiredNote(file: TFile): Promise<void> {
+		let root = this.app.vault.getRoot().path;
+		let destination = normalizePath(root + 'Trash/' + file.basename + '.' + file.extension);
+		console.log(destination);
+
+		await this.app.fileManager.renameFile(file, destination);
+	}
+
+	deleteExpiredNote(file: TFile): void {
+		this.app.vault.delete(file);
+	}
+
+	collectExpiredNotes(): TFile[] {
+		let collected: TFile[] = [];
+		let now = window.moment();
+
+		let allFiles = this.app.vault.getMarkdownFiles();
+		allFiles.forEach((file) => {
+			const metadata = this.app.metadataCache.getFileCache(file);
+			if(typeof metadata.frontmatter === 'undefined') {
+				return;
+			}
+
+			if (this.settings.frontmatterKey in metadata.frontmatter) {
+				let expiryDateString = metadata.frontmatter[this.settings.frontmatterKey];
+				let fileDate = window.moment(expiryDateString);
+
+
+				if (fileDate.isBefore(now)) {
+					collected.push(file);
+				}
+			}
+		});
+		
+		return collected;
+	}
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'expiring-notes-check',
+			name: 'Check for expired notes',
 			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+				this.checkForExpiredNotes();
 			}
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+		this.addSettingTab(new ExpiringNotesSettingTab(this.app, this));
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+
+		this.checkForExpiredNotes();
 	}
 
 	onunload() {
@@ -91,26 +108,10 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class ExpiringNotesSettingTab extends PluginSettingTab {
+	plugin: ExpiringNotesPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: ExpiringNotesPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -120,18 +121,32 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'Expiring Notes settings'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Frontmatter key')
+			.setDesc('Enter the key which you would like to use in your note\'s frontmatter to identify the note\'s expiry date.')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('expires')
+				.setValue(this.plugin.settings.frontmatterKey)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.frontmatterKey = value;
 					await this.plugin.saveSettings();
 				}));
+		
+		new Setting(containerEl)
+			.setName('Behavior')
+			.setDesc('Choose what should happen to your notes once they have expired.')
+			.addDropdown((d) => {
+				d.addOption("delete", "Delete file");
+				d.addOption("trash", "Move to trash folder");
+				d.setValue(this.plugin.settings.behavior);
+				d.onChange(async (v: "delete" | "trash") => {
+					this.plugin.settings.behavior = v;
+					await this.plugin.saveSettings();
+				});
+
+			});
+				
 	}
 }
